ad3074d9f2c869d8b21403caa43ce3e8c20b4ee4
Sergio Trett <serge3ling@gmail.com>
2019-12-31 18:06:02 +0200

    Zero functionality.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+class Operand extends React.Component {
+	render() {
+		return (
+			<div>
+			</div>
+		);
+	};
+}
 
+class Calc extends React.Component {
+	render() {
+		return (
+			<div>
+			Yes.
+			</div>
+		);
+	};
+}
+
+ReactDOM.render(<Calc />, document.getElementById("root"));
